1aacf4df8da67f80dc39f2d2e0cafcc8f3c7015b
Adriano <adrianosenny@gmail.com>
2023-03-21 08:30:32 -0300

    Criando dark-mode, falta alterar as cores corretamente

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".theme__btn");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", () => {
+  if (btn.style.left == "3%") {
+    btn.style.left = "61.5%";
+    btn.style.backgroundImage = "url(../../../assets/images/moon.svg)";
+    btn.style.transform = "rotate(180deg)";
+  } else {
+    btn.style.left = "3%";
+    btn.style.backgroundImage = "url(../../../assets/images/sun.svg)";
+    btn.style.transform = "rotate(0deg)";
+  }
+});
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
